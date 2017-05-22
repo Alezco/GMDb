25378b7d7c0e09e5160e3e41275a76ad103df0cb
WhiteDevil2013 <william.wakim@epita.fr>
2017-05-22 20:21:43 +0200

    create first login form

--- frontend/src/signIn.jsx
+++ frontend/src/signIn.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {render} from 'react-dom';
 const Redux =require('react-redux');
-
+import styles from './style/index.css';
 
 class SignIn extends Component {
 
@@ -12,14 +12,55 @@ class SignIn extends Component {
 
   render() {
     return(
-      <div>
-        <p>Fill the fields to sign in</p>
-            <form>
-              <input type="text" placeholder="Firstname"/><br/>
-              <input type="text" placeholder="Lastname"/><br/>
-              <input type="password" placeholder="Password"/>
+    <div>
+      <div className="form">
+          <ul className="tab-group">
+            <li className="tab active"><a href="#signup">Sign Up</a></li>
+            <li className="tab"><a href="#login">Log In</a></li>
+          </ul>
+
+          <div className="tab-content">
+            <div id="signup">
+              <h1>Sign Up for Free</h1>
+
+              <form action="/" method="post">
+                  <div className="field-wrap">
+                    <label>Login<span className="req">*</span></label>
+                    <input type="text"required autocomplete="off"/>
+                  </div>
+                  <div className="field-wrap">
+                    <label>Set A Password<span className="req">*</span></label>
+                    <input type="password"required autocomplete="off"/>
+                  </div>
+              <div className="field-wrap">
+                <label>Repeat The Password<span className="req">*</span></label>
+                <input type="password"required autocomplete="off"/>
+              </div>
+              <button type="submit" className="button button-block">Get Started</button>
             </form>
-       </div>
+          </div>
+
+          <div id="login">
+            <h1>Welcome Back!</h1>
+            <form action="/" method="post">
+                <div className="field-wrap">
+                  <label>Email Address<span className="req">*</span></label>
+                  <input type="email"required autocomplete="off"/>
+                </div>
+
+                <div className="field-wrap">
+                  <label>Password<span className="req">*</span></label>
+                  <input type="password"required autocomplete="off"/>
+                </div>
+                <p className="forgot"><a href="#">Forgot Password?</a></p>
+                <button className="button button-block">Log In</button>
+            </form>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
       );
   }
 }
