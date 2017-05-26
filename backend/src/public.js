728ec3dfdb15a6374092bd5d7500b428e84e00c2
WhiteDevil2013 <william.wakim@epita.fr>
2017-05-26 15:25:35 +0200

    remove broken star wars movie in the db

--- backend/src/public.js
+++ backend/src/public.js
@@ -3,7 +3,7 @@ const db = require('./db.js');
 
 exports.getFilms =  function(done) {
   console.log('get movies');
-  db.connection.query("select * from movies LIMIT 10", function(err,rows){
+  db.connection.query("select * from movies LIMIT 12", function(err,rows){
       if (err) {
         done(err, null);
       }
