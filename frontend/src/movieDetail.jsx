144556d3ba32d6becf36c36cd4c0f909fc98ca32
Hadrien de Lamotte <paultr_h@epita.fr>
2017-06-02 20:07:59 +0200

    Add video to detail movie page

--- frontend/src/movieDetail.jsx
+++ frontend/src/movieDetail.jsx
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 const Redux = require('react-redux');
 import styles from './style/index.css';
 import { withRouter } from 'react-router';
+import NavBar from './navBar.jsx';
 
 class MovieDetail extends Component {
 
@@ -54,45 +55,59 @@ class MovieDetail extends Component {
       return (<div></div>);
     }
     return (
-    <div>
-      <div className="container">
-		<div className="card">
-			<div className="container-fliud">
-				<div className="wrapper row">
-      <div className="col-md-6">
-						  <img className="center-img" src={this.state.movieObject.Poster} />
+      <div>
+        <NavBar />
+     <div className="container">
+        <div className="card">
+           <div className="container-fliud">
+              <div className="wrapper row">
+                 <div className="col-md-6">
+                    <img className="center-img" src={this.state.movieObject.Poster} />
+                 </div>
+                 <div className="details col-md-6">
+                    <h3 className="product-title">{this.state.movieObject.Title}</h3>
+                    <div className="rating">
+                       <div className="stars">
+                          <span className="fa fa-star checked"></span>
+                          <span className="fa fa-star checked"></span>
+                          <span className="fa fa-star checked"></span>
+                          <span className="fa fa-star"></span>
+                          <span className="fa fa-star"></span>
+                       </div>
+                       <span className="review-no">41 reviews</span>
+                    </div>
+                    <p className="product-description">Suspendisse quos? Tempus cras iure temporibus? Eu laudantium cubilia sem sem! Repudiandae et! Massa senectus enim minim sociosqu delectus posuere.</p>
+                    <h4 className="price">current price: <span>$ 299</span></h4>
+                    <p className="vote"><strong>91%</strong> of buyers enjoyed this product! <strong>(87 votes)</strong></p>
+                    <h5 className="sizes">sizes:
+                       <span className="size" data-toggle="tooltip" title="small">s</span>
+                       <span className="size" data-toggle="tooltip" title="medium">m</span>
+                       <span className="size" data-toggle="tooltip" title="large">l</span>
+                       <span className="size" data-toggle="tooltip" title="xtra large">xl</span>
+                    </h5>
+                    <div className="action">
+                       <button className="add-to-cart btn btn-default" type="button">add to cart</button>
+                       <button className="like btn btn-default" type="button"><span className="fa fa-heart"></span></button>
+                    </div>
+                 </div>
+
               </div>
-					<div className="details col-md-6">
-						<h3 className="product-title">{this.state.movieObject.Title}</h3>
-						<div className="rating">
-							<div className="stars">
-								<span className="fa fa-star checked"></span>
-								<span className="fa fa-star checked"></span>
-								<span className="fa fa-star checked"></span>
-								<span className="fa fa-star"></span>
-								<span className="fa fa-star"></span>
-							</div>
-							<span className="review-no">41 reviews</span>
-						</div>
-						<p className="product-description">Suspendisse quos? Tempus cras iure temporibus? Eu laudantium cubilia sem sem! Repudiandae et! Massa senectus enim minim sociosqu delectus posuere.</p>
-						<h4 className="price">current price: <span>$ 299</span></h4>
-						<p className="vote"><strong>91%</strong> of buyers enjoyed this product! <strong>(87 votes)</strong></p>
-						<h5 className="sizes">sizes:
-							<span className="size" data-toggle="tooltip" title="small">s</span>
-							<span className="size" data-toggle="tooltip" title="medium">m</span>
-							<span className="size" data-toggle="tooltip" title="large">l</span>
-							<span className="size" data-toggle="tooltip" title="xtra large">xl</span>
-						</h5>
-						<div className="action">
-							<button className="add-to-cart btn btn-default" type="button">add to cart</button>
-							<button className="like btn btn-default" type="button"><span className="fa fa-heart"></span></button>
-						</div>
-					</div>
-          </div>
-          </div>
-          </div>
+              <div className="wrapper row">
+                <div className="center-img videoPadding">
+                <p>Trailer of the movie</p>
+                <iframe
+                    allowFullScreen
+                    frameBorder="0"
+                    height="420"
+                    src={this.state.movieObject.Youtube}
+                    width="746"
+                  />
+              </div>
+              </div>
+           </div>
         </div>
-      </div>
+     </div>
+  </div>
   );
 }
 
